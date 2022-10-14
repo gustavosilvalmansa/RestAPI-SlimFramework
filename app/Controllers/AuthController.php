ea1cf4668469648f38b015b6c4dcb54c07ee8912
Gustavo Almansa <gustavosalmansa@gmail.com>
2022-10-14 18:00:20 -0300

    Add Jwt token to project

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use App\Dao\MySQL\GerenciadorDeLojas\UsuariosDao;
 use App\Models\MySQL\GerenciadorDeLojas\UsuarioModel;
+use Firebase\JWT\JWT;
 
 final class AuthController{ // Ngm herda
 	public function login(Request $request, Response $response, array $args): Response{
@@ -26,8 +27,21 @@ final class AuthController{ // Ngm herda
 			return $response->withStatus(401);
 		}
 		
-		var_dump($usuario);
+		$tokenPayLoad = [
+			'sub'=>$usuario->getId(),
+			'name'=>$usuario->getNome(),
+			'email'=>$usuario->getEmail(),
+			'expired_at'=>(new \DateTime())->modify('+ 2 days')->format('Y-m-d H:i:s')
+		];
 		
+		$token = JWT::encode($tokenPayLoad, getenv('JWT_SECRET_KEY'));
+		
+		$refreshTokenPayload = [
+			'email' => $usuario->getEmail()
+		];
+		$refreshToken = JWT::encode($refreshTokenPayload, getenv('JWT_SECRET_KEY'));
+		
+		var_dump($token);
 		return $response;
 	}
 
